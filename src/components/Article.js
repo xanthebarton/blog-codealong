35b19a741a26094bd87ec76207b182721333ae1c
xanthebarton <xanthe0220@gmail.com>
2022-11-11 18:52:51 -0800

    date

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,8 +6,8 @@ export default function Article({ article }) {
       ) : (
         <section>
           <h2>{article.title}</h2>
-          <p className="date">{`Posted: ${article.date}`}</p>
           <p className="body">{article.body}</p>
+          <p className="date">`Posted:' {new Date()}</p>
         </section>
       )}
     </article>
